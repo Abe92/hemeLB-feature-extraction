8eb83653827bebd13970aa2fcaf784da52572723
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2016-08-04 10:51:03 +0100

    Update 1238073-time_steps.py

--- Scripts/Calculator/1238073-time_steps.py
+++ Scripts/Calculator/1238073-time_steps.py
@@ -11,15 +11,23 @@ import numpy as np
 import csv
 
 #1
-x = np.zeros([0,1])
+x = np.zeros([0,1])  # Replace '1' with the number of columns in your csv file
 
 #2
-with open ('D:/Python/In/time_steps-and-pressure/hemeLB-time_steps.txt') as csvfile:
+with open ('csv_file_name') as csvfile:
     file = csv.reader(csvfile)
     
     #3
     for row in file:
         x = np.vstack([x,[float(row[0])]])
+        
+        """
+        For more than 1 column:
+        x = np.vstack([x,[float(row[0]), float(row[1]), ...(..)]])
+        
+        ! The number inside [float(row[number])] indicates the index number of columns.
+        ! Remember that index number in computer start from 0
+        """
       
 #4
 seconds = ((x[:,0])/1000) % 60
