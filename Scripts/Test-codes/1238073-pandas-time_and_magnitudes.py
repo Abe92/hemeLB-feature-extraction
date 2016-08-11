de81333b5b403e62e84e0e833d6f0e8f7fb7692f
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2016-08-11 18:29:27 +0100

    Update 1238073-pandas-time_and_magnitudes.py

--- Scripts/Test-codes/1238073-pandas-time_and_magnitudes.py
+++ Scripts/Test-codes/1238073-pandas-time_and_magnitudes.py
@@ -9,8 +9,8 @@ import pandas as pd
 import numpy as np
 
 # Pandas configuration
-pd.set_option('display.height', 'height_value')
-pd.set_option('display.max_rows', 'rows_value') 
+pd.set_option('display.height', 'height_value')   
+pd.set_option('display.max_rows', 'rows_value')   # 'rows_value' should be adjusted to the number of rows from the csv file
 
 # Source file
 df = pd.read_csv('csv-file-name')
@@ -34,12 +34,12 @@ magnitudes = str(magnitudes)    # optional, but useful if you want to save the o
 print(seconds)
 print(magnitudes)
 
-# Seconds
-#with open("D:\\Python\\In\\csv\\time-steps.txt", 'w') as time:    
-#        for item in seconds:
-#            time.write(seconds)
-#
-# Magnitudes
-#with open("D:\\Python\\In\\csv\\magnitudes.txt", 'w') as magnitude:    
-#        for item in magnitudes:
-#            magnitude.write(magnitudes)
+# Save the output to text file
+#with open("D:\\Python\\In\\csv\\time-steps.txt", 'w') as time:             # Known issues:
+#        for item in seconds:                                               # (1) It does save all the information from the calculator code.
+#            time.write(seconds)                                            # However, the current state doesn't have some 'boundaries'
+#                                                                           # to tell the code when to stop writing the calculation.
+# Magnitudes                                                                #
+#with open("D:\\Python\\In\\csv\\magnitudes.txt", 'w') as magnitude:        # 
+#        for item in magnitudes:                                            # 
+#            magnitude.write(magnitudes)                                    #
