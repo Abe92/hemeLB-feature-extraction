948e6d73c5b760c2c71a0ce3657db46db0043a32
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2016-08-12 11:42:16 +0100

    Update 1238073-plots.py

--- Scripts/Plots/1238073-plots.py
+++ Scripts/Plots/1238073-plots.py
@@ -13,7 +13,7 @@ import csv
 xy = np.zeros([0,2]) # Replace '2' with the number of columns in your csv file
 
 #2
-with open ('your-csv-file-name') as csvfile:
+with open ('hemeLB-csv-file-name') as csvfile:
     file = csv.reader(csvfile)
     
     #3
