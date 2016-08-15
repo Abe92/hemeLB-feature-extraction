57ee0b2c5eaf1f5455d63986019c16cbb9635b7d
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2016-08-15 22:01:37 +0100

    Add files via upload

---
+++ Scripts/Feature-extraction/feature-extraction.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Date created : 15 August 2016
+@author      : Aldy Rasyid Abe
+@description : HemeLB-feature-extraction-manual_approach
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+- Requirements: 
+(1) The latest Python (the program was written in 3.0)
+(2) hemeLB text file acquired from hemeXtract
+
+-The program utilising the Pandas library.
+
+-File types supported by the Pandas
+(1) Flat files (CSV and delimited),
+(2) Excel files,
+(3) Databases and
+(4) HDF5.
+
+-The program primarily focused on the flat files type.
+
+-Please consult to {http://pandas.pydata.org/pandas-docs/stable/io.html}
+for more details of using other file types.
+"""
+
+## Load your file here
+df = pd.read_csv('hemeLB-flat_file-name') # Flat files (CSV and delimited)
+
+## List your column of the flat files here
+time_step = df['step']
+
+grid_X = df['X']
+grid_Y = df['Y']
+grid_Z = df['Z']
+
+velocity0 = df['velocity(0)']
+velocity1 = df['velocity(1)']
+velocity2 = df['velocity(2)']
+
+magnitude = df['magnitudes']  # I added this column manually after converted
+                              # the file into CSV from text file
+pressure = df['pressure']
+
+## Calculator
+# (1) Converting the value of time from column step into seconds
+seconds = (time_step*4.00E-06)
+seconds.to_csv(r'file-output-name', header=None, index=None, sep=',', mode='a')    # save the conversion of time to file
+
+# (2) Finding the magnitudes of the velocity vectors
+magnitudes = np.sqrt(velocity0*velocity0 + velocity1*velocity1 + velocity2*velocity2)
+magnitudes.to_csv(r'file-output-name', header=None, index=None, sep=',', mode='a') # save the calculation of magnitudes to file
+
+## Plotting
+# (1) Regular plot
+plt.plot('column-name1', 'column-name2', label='regular-plot-label', color='#0066cc')
+plt.xlabel('x_axes-label')
+plt.ylabel('y_axes-label')
+plt.title('regular-plot-title')
+plt.grid(True)
+plt.legend()
+plt.show()
+
+# (2) Scatter plot
+plt.scatter('column-name1', 'column-name2', label='regular-plot-label', color='#0066cc')
+plt.xlabel('x_axes-label')
+plt.ylabel('y_axes-label')
+plt.title('regular-plot-title')
+plt.grid(True)
+plt.legend()
+plt.show()
