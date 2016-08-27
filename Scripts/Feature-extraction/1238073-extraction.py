20fd4fd368b6c7f55927c7256b11efca53ab5a82
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2016-08-27 11:06:54 +0100

    Add files via upload
    The new building block code for manual feature extraction

---
+++ Scripts/Feature-extraction/1238073-extraction.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Date created : 27 August 2016
+@author      : Aldy Rasyid Abe
+@description : Create a new flat file with its modified features
+"""
+
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+
+## Source file
+df = pd.read_csv('D:/Python/In/csv/dummy.csv')
+
+## List of original column names from the flat file
+step = df['step']
+gridx = df['grid_x']
+gridy = df['grid_y']
+gridz = df['grid_z']
+velocity0 = df['velocity(0)']
+velocity1 = df['velocity(1)']
+velocity2 = df['velocity(2)']
+pressure = df['pressure']
+
+
+"""
+                            Calculator
+"""
+# (1) Converting the dimensionless step value into step value with unit of seconds
+time_step = (step * 4.00E-06)
+
+#(2) Finding the magnitudes of the velocity vectors
+magnitude = np.sqrt(velocity0*velocity0 + velocity1*velocity1 + velocity2*velocity2)
+
+
+"""
+                    Creating new data frame 
+"""
+# (1) Create a dictionary
+# List all of the columns including its values
+raw_data = {'step': time_step,
+          'grid_x':gridx,
+          'grid_y':gridy,
+          'grid_z':gridz,
+          'velocity(0)':velocity0,
+          'velocity(1)':velocity1,
+          'velocity(2)':velocity2,
+          'pressure':pressure,
+          'magnitudes':magnitude}
+
+# (2) Create the new dataframe from the dictionary
+new_df = pd.DataFrame(raw_data,columns = ['step','grid_x','grid_y','grid_z','velocity(0)','velocity(1)','velocity(2)','pressure','magnitudes'])
+
+
+"""
+            Transform the new dataframe into flat file (CSV)
+"""
+new_df.to_csv(r'D:/Python/In/csv/test_dummy.csv', header=True, index=None, sep=',', mode='a')
