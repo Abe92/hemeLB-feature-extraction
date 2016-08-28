d4ee874075afb48a816d4ec56a71af8aedf7b978
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2016-08-28 18:34:25 +0100

    Update 1238073-CLEANING.py

--- Scripts/Feature-extraction/1238073-CLEANING.py
+++ Scripts/Feature-extraction/1238073-CLEANING.py
@@ -3,18 +3,28 @@
 Date created : 27 August 2016
 @author      : Aldy Rasyid Abe
 @description : Data cleaning - POSTPONED
+@update      : 28 August 2016, still bugged don't use it
 """
 
 import fileinput
 
 # Approach 1
-with open (r'D:\\Python\In\csv\new_planeOutTopFirst.txt') as f:
+with open (r'path/to/file.csv') as f:
     for line in f:
         cleanedLine = line.strip()
         if cleanedLine: # is not empty
             print(cleanedLine)
             
 # Approach 2
-for line in fileinput.FileInput(r'D:\\Python\In\csv\new_planeOutTopFirst.txt', inplace=1):
+for line in fileinput.FileInput(r'path/to/file.csv', inplace=1): 
     if line.rstrip('\n'):
         print (line)
+
+"""
+Notes for Approach 2
+"""
+# inplace=1,
+# The file is moved to a backup file and standard output  
+# is directed to the input file 
+# (if a file of the same name as the backup file already exists, 
+# it will be replaced silently)
